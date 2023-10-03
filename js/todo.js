5d37edd5da332dcc58cc06c73639112b7ceba4a0
wipddsu <117911142+wipddsu@users.noreply.github.com>
2023-10-04 00:08:44 +0900

    모멘텀 클론 코팅 바닐라 JS 기능 구현 마무리

---
+++ js/todo.js
@@ -0,0 +1,59 @@
+const toDoForm = document.getElementById('todo-form');
+const toDoInput = toDoForm.querySelector('input');
+const toDoList = document.getElementById('todo-list');
+
+const TODOS_KEY = 'todos';
+
+let toDos = [];
+
+const saveToDos = function () {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+};
+
+const deleteToDo = function (event) {
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+  saveToDos();
+};
+
+const paintToDo = function (newTodo) {
+  const li = document.createElement('li');
+  li.id = newTodo.id;
+  const span = document.createElement('span');
+  span.innerText = newTodo.text;
+  const button = document.createElement('button');
+  button.innerText = '❌';
+  button.addEventListener('click', deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+};
+
+const handleToDoSubmit = function (event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = '';
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
+};
+
+toDoForm.addEventListener('submit', handleToDoSubmit);
+
+//   const sayHello = function (item) {
+//     console.log('This is a turn of', item);
+//   };
+//   item => console.log('This is a turn of', item)
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
